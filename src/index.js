4adb967ea5fb8a62ea4b981083f91d0061f739e9
LaurentiuSorinTENE <sorin.tene@gmail.com>
2024-10-30 15:37:18 +0200

    complet functionala

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { App } from 'components/App';
+import './index.css';
+import { ToastContainer } from 'react-toastify';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
-// import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { persistor, store } from './reduxConfig/store';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename='/project-Pokemons'>
+        <App />
+        <ToastContainer
+          position='top-center'
+          autoClose={3000}
+          hideProgressBar={true}
+          newestOnTop={true}
+          closeOnClick
+          theme='colored' />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>);
