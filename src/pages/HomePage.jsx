80679add0e170a6c71d206f4d9fbd057a02a73d1
DorinGI <158717197+DorinGI@users.noreply.github.com>
2024-10-30 22:01:53 +0100

    error fix from Alex

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getTransactions } from '../redux/transactions/operations';
-import { getTransactionsCategories } from './StatisticsPage';
+import { fetchAllTransactions } from '../redux/transactions/operations';
+// import { getTransactionsCategories } from './StatisticsPage';
 import TransactionList from './common/components/TransactionsList';
 import AddButton from './common/components/AddButton/AddButton';
 function Home() {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getTransactions());
-    dispatch(getTransactionsCategories());
+    dispatch(fetchAllTransactions());
+    // dispatch(getTransactionsCategories());
   }, [dispatch]);
   return (
     <>
